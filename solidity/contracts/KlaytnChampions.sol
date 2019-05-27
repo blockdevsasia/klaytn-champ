8531acc5f4d205b813c8d2a2af68e28e972fcd83
Caspar Oostendorp <caspar@oostvoort.com>
2019-05-27 08:26:05 +0800

    Worked on contract

--- solidity/contracts/KlaytnChampions.sol
+++ solidity/contracts/KlaytnChampions.sol
@@ -8,15 +8,26 @@ contract Champions {
         uint256 level;
     }
 
-    mapping(address => PlayerData) players;
+    // The main mapping that contains the data for each player
+    mapping(address => PlayerData) _players;
 
     function registerPlayer(address playerAddress, uint256 randomAmount) public {
-        // TODO Bail if playerAddress already exists
-        // TODO Create new struct
-        // TODO Store new struct in mapping
+        require(
+            _players[playerAddress].level == 0,
+            "Cannot register twice"
+        );
+
+        PlayerData memory newPlayer =  PlayerData(randomAmount, 1);
+
+        _players[playerAddress] = newPlayer;
+
     }
 
-    function getPlayer(address playerAddress) public view {
-        // TODO Return struct from mapping for playerAddress
+    function getPlayer(address playerAddress)
+    public view
+    returns (uint256 randomAmount, uint256 level)
+    {
+        PlayerData storage player = _players[playerAddress];
+        return (player.randomAmount, player.level);
     }
 }
