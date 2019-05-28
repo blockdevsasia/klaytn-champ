3af9c594befb8f1778fa7111c564b85a50c4c213
Caspar Oostendorp <caspar@oostvoort.com>
2019-05-28 15:36:22 +0800

    Adapting for new layouts

---
+++ client/src/layouts/AuthLayout.vue
@@ -0,0 +1,46 @@
+<template>
+  <q-layout view="lHh Lpr lFf">
+    <q-drawer
+      v-model="leftDrawerOpen"
+      bordered
+      content-class="bg-grey-2"
+      :width="400"
+    >
+      <router-view></router-view>
+    </q-drawer>
+
+  </q-layout>
+</template>
+
+<script>
+import { openURL } from 'quasar'
+import { mapState } from 'vuex'
+
+export default {
+  name: 'AuthLayout',
+  mounted: function () {
+    console.log(this.$router)
+  },
+  components: {
+  },
+  data () {
+    return {
+      tab: 'level',
+      leftDrawerOpen: this.$q.platform.is.desktop
+    }
+  },
+  computed: {
+    ...mapState('user', {
+      currentUser: 'current',
+      level: 'level'
+    })
+
+  },
+  methods: {
+    openURL
+  }
+}
+</script>
+
+<style>
+</style>
---
+++ client/src/layouts/LevelLayout.vue
@@ -0,0 +1,73 @@
+<template>
+  <q-layout view="lHh Lpr lFf">
+    <q-header elevated>
+
+      <q-toolbar>
+
+        <q-btn
+          flat
+          dense
+          round
+          @click="leftDrawerOpen = !leftDrawerOpen"
+          aria-label="Menu"
+          class="menu-bar"
+        >
+          <q-icon name="menu" />
+        </q-btn>
+        <q-toolbar-title>
+          Klaytn Champ: Level {{level}}
+        </q-toolbar-title>
+
+      </q-toolbar>
+    </q-header>
+
+    <q-drawer
+      v-model="leftDrawerOpen"
+      bordered
+      content-class="bg-grey-2"
+      :width="400"
+    >
+      <level-side-bar></level-side-bar>
+    </q-drawer>
+
+    <q-page-container>
+
+      <router-view></router-view>
+    </q-page-container>
+  </q-layout>
+</template>
+
+<script>
+import { openURL } from 'quasar'
+import LevelSideBar from 'components/LevelSideBar'
+import { mapState } from 'vuex'
+
+export default {
+  name: 'MyLayout',
+  mounted: function () {
+    console.log(this.$router)
+  },
+  components: {
+    LevelSideBar: LevelSideBar
+  },
+  data () {
+    return {
+      tab: 'level',
+      leftDrawerOpen: this.$q.platform.is.desktop
+    }
+  },
+  computed: {
+    ...mapState('user', {
+      currentUser: 'current',
+      level: 'level'
+    })
+
+  },
+  methods: {
+    openURL
+  }
+}
+</script>
+
+<style>
+</style>
