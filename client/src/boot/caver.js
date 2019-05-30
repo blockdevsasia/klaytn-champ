5c691c465f36e8d422840b89c3ed41ce1c9fb2cb
Caspar Oostendorp <caspar@oostvoort.com>
2019-05-30 14:58:18 +0800

    Several fixes. Added temporary reset function.

--- client/src/boot/caver.js
+++ client/src/boot/caver.js
@@ -2,7 +2,7 @@ import Caver from 'caver-js'
 const Contract = require('src/assets/contracts/KlaytnChamp.json')
 
 const RPC_URL = 'https://api.baobab.klaytn.net:8651'
-const CONTRACT_ID = '0x48667D376fc9E5E8D19a6FCeB64C8d4c6E4553b8'
+const CONTRACT_ID = '0x745d6af8643C9b37f78Fd762255f9c0A81650707'
 
 export const CAVER = new Caver(RPC_URL)
 
