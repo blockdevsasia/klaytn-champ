86d7818524be8a4c11f360540c53e12838702ffe
Caspar Oostendorp <caspar@oostvoort.com>
2019-05-30 13:15:45 +0800

    Small fix to link in Index

--- client/src/pages/Index.vue
+++ client/src/pages/Index.vue
@@ -1,70 +1,74 @@
 <template>
   <q-page class="content-container">
     <h6>Introduction</h6>
-    <p class="content-container">Below you can find an overview of the different challenges from Klay Kakao’s global public blockchain project Klaytn is an enterprise-grade, service-centric platform that brings user-friendly blockchain experience to millions. It combines the best features of both public blockchains (decentralized data & control, distributed governance) and private blockchains (low latency, high scalability) via an efficient ‘hybrid’ design.
+    <p class="content-container">Below you can find an overview of the different challenges from Klay Kakao’s global
+      public blockchain project Klaytn is an enterprise-grade, service-centric platform that brings user-friendly
+      blockchain experience to millions. It combines the best features of both public blockchains (decentralized data &
+      control, distributed governance) and private blockchains (low latency, high scalability) via an efficient ‘hybrid’
+      design.
     </p>
 
-<div class="q-pa-md">
+    <div class="q-pa-md">
       <div class="row">
-      	<div class="col home-button left-hb">
-        	<div class="row">
-				<div class="col-12 col-md-4">
-					<router-link to="/level/1">
-        			<img class="btnimg-dash center" src="assets\klaytn-basics.png" alt="" />
-        			</router-link>
-        		</div>
-    			<div class="col-12 col-md-8">
-    				<p class="text-weight-bolder">Mastering the basic</p>
-    				<p>Get started with this challenge to learn
-					the basic set-up with the Klaytn token.</p>
-				</div>
-			</div>
+        <div class="col home-button left-hb">
+
+          <router-link to="/level/1">
+            <div class="row">
+              <div class="col-12 col-md-4">
+                <img class="btnimg-dash center" src="assets/klaytn-basics.png" alt=""/>
+
+              </div>
+              <div class="col-12 col-md-8">
+                <p class="text-weight-bolder">Mastering the basic</p>
+                <p>Get started with this challenge to learn
+                  the basic set-up with the Klaytn token.</p>
+              </div>
+            </div>
+          </router-link>
+        </div>
+
+        <div class="col home-button">
+          <div class="row">
+            <div class="col-12 col-md-4">
+              <img class="btnimg-dash center" src="assets/klaytn-advanced.png" alt=""/>
+            </div>
+            <div class="col-12 col-md-8">
+              <p class="text-weight-bolder">Diving into advanced methods</p>
+              <p>Start this challenge to get your skills to
+                the next level.</p>
+            </div>
+          </div>
+        </div>
       </div>
 
-      	<div class="col home-button">
-        	<div class="row">
-				<div class="col-12 col-md-4">
-        			<img class="btnimg-dash center" src="assets\klaytn-advanced.png" alt="" />
-        		</div>
-    			<div class="col-12 col-md-8">
-    				<p class="text-weight-bolder">Diving into advanced methods</p>
-    				<p>Start this challenge to get your skills to
-					the next level.</p>
-				</div>
-			</div>
+      <div class="row">
+        <div class="col home-button left-hb">
+          <div class="row">
+            <div class="col-12 col-md-4">
+              <img class="btnimg-dash center" src="assets/klaytn-expert.png" alt=""/>
+            </div>
+            <div class="col-12 col-md-8">
+              <p class="text-weight-bolder">For the experts</p>
+              <p>Are you ready for a big challenge?
+                Let’s get started!</p>
+            </div>
+          </div>
+        </div>
+
+        <div class="col home-button">
+          <div class="row">
+            <div class="col-12 col-md-4">
+              <img class="btnimg-dash center" src="assets/klaytn-champ-logo.png" alt=""/>
+            </div>
+            <div class="col-12 col-md-8">
+              <p class="text-weight-bolder">Much more to come!</p>
+              <p>Stay tuned for the latest courses and challances and earn real rewards!</p>
+            </div>
+          </div>
+        </div>
       </div>
+
     </div>
-
-     <div class="row">
-      	<div class="col home-button left-hb">
-        	<div class="row">
-				<div class="col-12 col-md-4">
-        			<img class="btnimg-dash center" src="assets\klaytn-expert.png" alt="" />
-        		</div>
-    			<div class="col-12 col-md-8">
-    				<p class="text-weight-bolder">For the experts</p>
-    				<p>Are you ready for a big challenge?
-    				Let’s get started!</p>
-				</div>
-			</div>
-      </div>
-
-      	<div class="col home-button">
-        	<div class="row">
-				<div class="col-12 col-md-4">
-        			<img class="btnimg-dash center" src="assets\klaytn-champ-logo.png" alt="" />
-        		</div>
-    			<div class="col-12 col-md-8">
-    				<p class="text-weight-bolder">Much more to come!</p>
-    				<p>Stay tuned for the latest courses and challances and earn real rewards!</p>
-				</div>
-			</div>
-      </div>
-    </div>
-
-
-
-</div>
   </q-page>
 
 </template>
@@ -73,7 +77,7 @@
 </style>
 
 <script>
-export default {
-  name: 'PageIndex'
-}
+  export default {
+    name: 'PageIndex'
+  }
 </script>
