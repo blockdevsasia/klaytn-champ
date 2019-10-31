8a9b24ee9cdd1e4a59b5656371f8d733dc749bd5
Caspar Oostendorp <caspar@oostvoort.com>
2019-10-31 09:09:02 +0800

    Remove service account from vcs

--- server/firebaseHelpers.js
+++ server/firebaseHelpers.js
@@ -1,6 +1,6 @@
 
 var admin = require('firebase-admin')
-var serviceAccount = require('./serviceAccountKey.json')
+var serviceAccount = require('./.gcpServiceAccount.json')
 
 admin.initializeApp({
   credential: admin.credential.cert(serviceAccount),
